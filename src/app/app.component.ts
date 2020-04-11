b59ce41302dd774a5899c7fc8765511cf192cfbc
lgarcin <laurent.garcin@gmail.com>
2020-04-11 13:16:04 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Component } from "@angular/core";
+import { render } from 'katex'
+import { KatexOptions } from 'ng-katex';
+
+import questions from '../app/questions/questions.json'
 
 @Component({
   selector: "app-root",
@@ -6,9 +10,45 @@ import { Component, ViewChild, ElementRef } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  test: string;
-  objectif = "a^2+b^2=c^2";
 
-  @ViewChild("#res") resdiv: ElementRef;
-  @ViewChild("#obj") objdiv: ElementRef;
+  nb = 0;
+  test = "";
+  questions = questions;
+  options: KatexOptions = {
+    displayMode: true,
+    throwOnError: false
+  };
+
+  get objectif() {
+    return this.questions[this.nb];
+  }
+
+  decrease() {
+    this.nb--;
+    this.test = "";
+  }
+
+  increase() {
+    this.nb++;
+    this.test = "";
+  }
+
+  testEquality() {
+    try {
+      let testElement = document.createElement('object');
+      render(this.test, testElement, { throwOnError: false });
+      testElement = testElement.querySelector('semantics');
+      testElement.removeChild(testElement.querySelector('annotation'))
+
+      let objectifElement = document.createElement('object');
+      render(this.objectif, objectifElement, { throwOnError: false });
+      objectifElement = objectifElement.querySelector('semantics');
+      objectifElement.removeChild(objectifElement.querySelector('annotation'))
+
+      return testElement.isEqualNode(objectifElement);
+    }
+    catch {
+      return false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from "@angular/forms";
 import { KatexModule } from "ng-katex";
 
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, KatexModule],
+  imports: [BrowserModule, FormsModule, KatexModule, BrowserAnimationsModule, MatButtonModule, MatProgressBarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
